3b02454410e9c685dddabc34b3f4abb6bf3496a7
AnnaFutryk <anna.futryk@gmail.com>
2023-07-08 17:16:55 +0300

    Update ContactList.styled.js

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const List = styled.ul`
   display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  width: 600px;
   justify-content: center;
   flex-direction: column;
-  padding: 12px;
   list-style: none;
 `;
